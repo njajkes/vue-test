6ff7c7dece90f71872d74624490052b92a554c86
njajkes <naikes.nikolait@yandex.ru>
2022-06-21 02:54:51 +0300

    add sass + template for all components

---
+++ components/CardList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <product-card />
+  </div>
+</template>
+
+<script>
+import ProductCard from './ProductCard.vue'
+export default {
+  components: { ProductCard }
+
+}
+</script>
+
+<style>
+
+</style>
